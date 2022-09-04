4e1637db4624caf4b56794563e761d029d056572
Nicholas Torres <nicholastorres@gmx.com>
2022-09-04 17:44:09 -0400

    beginnings

---
+++ src/main/java/com/darkstoryauction/wikiauctions/WikiAuctionsApplication.java
@@ -0,0 +1,13 @@
+package com.darkstoryauction.wikiauctions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikiAuctionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WikiAuctionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/darkstoryauction/wikiauctions/WikiAuctionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darkstoryauction.wikiauctions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikiAuctionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
